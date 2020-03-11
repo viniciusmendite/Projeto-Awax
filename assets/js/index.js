6f2a11fcb4ce5c55d3f863bae4687b1debd9a2e1
Vinicius Mendite <vinicius.miff@gmail.com>
2020-03-11 08:39:40 -0300

    Conclusão do site em si, falta fazer a responsividade

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+// animation of scrool
+$("nav a").click(function (e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+		menuHeight = $('nav').innerHeight(),
+		targetOffset = $(id).offset().top;
+	$('html, body').animate({
+		scrollTop: targetOffset - menuHeight
+	}, 1000);
+})(JQuery);
